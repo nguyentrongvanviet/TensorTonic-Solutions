d3507453b199326ab309517c24e5487162839468
Nguyễn Trọng Văn Viết <nguyentrongvanviet@gmail.com>
2026-04-14 00:39:24 +0700

    Update solution for dropout-training

--- dropout-training/dropout-training.py
+++ dropout-training/dropout-training.py
@@ -9,9 +9,10 @@ def dropout(x, p=0.5, rng=None):
     # Write code here
     if p == 1 :
         return np.zeros(x.shape),np.zeros(x.shape)
-    mask = np.random.random(x.shape) 
-    if rng != None :
-        mask = rng.random(x.shape) # doan bua 
+    mask = np.random.random(x.shape) if rng==None else rng.random(x.shape)
+    # mask = np.random.random(x.shape) 
+    # if rng != None :
+    #     mask = rng.random(x.shape) # doan bua 
     mask = np.where(mask>=p,1,0)
     oldx = x 
     x = x*(mask/(1-p))
